e74304d0d4edd664dc44f0ed46fde568f60e3798
Andres <AndresEM1524@gmail.com>
2023-06-18 13:46:13 -0700

    adding hero background, fixing css for title, and changing typography colors

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,72 +3,30 @@ import Typography from '@mui/material/Typography';
 import './Styles/Hero.css';
 
 const Hero = () => {
-  // const navbarLinkStyleTopLeft = {
-  //   position: 'absolute',
-  //   top: '2rem',
-  //   left: '2rem',
-  //   textDecoration: 'none', // Add this line to remove text decoration
-  // };
-
-  // const navbarLinkStyleTopRight = {
-  //   position: 'absolute',
-  //   top: '2rem',
-  //   right: '2rem',
-  //   textDecoration: 'none',
-  // };
-
-  // const navbarLinkStyleBottomLeft = {
-  //   position: 'absolute',
-  //   bottom: '2rem',
-  //   left: '2rem',
-  //   textDecoration: 'none',
-  // };
-
-  // const navbarLinkStyleBottomRight = {
-  //   position: 'absolute',
-  //   bottom: '2rem',
-  //   right: '2rem',
-  //   textDecoration: 'none',
-  // };
-
-  // const mainTextContainerStyle = {
-  //   textAlign: 'center',
-  //   marginTop: '40vh',
-  // };
-
-  // const mainTextStyle = {
-  //   fontSize: '3rem',
-  //   fontWeight: 'bold',
-  //   marginBottom: '1rem',
-  // };
-
-  // const subTextStyle = {
-  //   fontSize: '1.5rem',
-  // };
 
   return (
     <div className='hero-container'>
       <div className='hero-container__nav-top-left'>
-        <a >About me</a>
+        <p>About me</p>
       </div>
 
       <div className='hero-container__nav-top-right'>
-        <a >My experience</a>
+        <p>My experience</p>
       </div>
 
       <div className='hero-container__nav-bottom-left' >
-        <a >Projects</a>
+        <p>Projects</p>
       </div>
 
       <div className='hero-container__nav-bottom-right'>
-        <a>Contact me!</a>
+        <p>Contact me!</p>
       </div>
 
       <div className='hero-container__text__container'>
-        <Typography variant="h1" className='.hero-container__title'>
+        <Typography variant="h1" className='.hero-container__title' color="white">
           Hello friend, I'm Andres
         </Typography>
-        <Typography variant="h3" className='.hero-container__subtext'>
+        <Typography variant="h3" className='.hero-container__subtext' color="white">
           A web developer
         </Typography>
       </div>
