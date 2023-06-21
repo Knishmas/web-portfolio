90370f5be95c44945bc4a8a6d2c0f56b274ab596
Andres <AndresEM1524@gmail.com>
2023-06-21 13:57:48 -0700

    created skills component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as Navbar} from './Navbar';
 export {default as AboutMe} from './AboutMe';
 export {default as Hero} from './Hero';
 export {default as Experiences} from './Experiences';
+export {default as Skills} from './Skills';
