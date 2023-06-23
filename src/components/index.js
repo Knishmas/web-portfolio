8fbe4d023044221bf51dcbfa3d3f819d0cd3e576
Andres <AndresEM1524@gmail.com>
2023-06-23 12:05:22 -0700

    Contact component added to index file

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {default as Hero} from './Hero';
 export {default as Experiences} from './Experiences';
 export {default as Skills} from './Skills';
 export {default as Projects} from './Projects';
+export {default as Contact} from './Contact';
