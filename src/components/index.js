a56e66572ac287f10e8643d22fe1362220d88381
Andres <AndresEM1524@gmail.com>
2023-06-22 22:24:33 -0700

    Created Project component and added to components index

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as AboutMe} from './AboutMe';
 export {default as Hero} from './Hero';
 export {default as Experiences} from './Experiences';
 export {default as Skills} from './Skills';
+export {default as Projects} from './Projects';
