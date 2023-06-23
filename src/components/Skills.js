67d4a7af80967cc7c34e8836f6011f6f1f98befa
Andres <AndresEM1524@gmail.com>
2023-06-22 20:31:23 -0700

    Created a responsive grid of icons with MUI

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Container, Grid } from '@mui/material';
+import './Styles/Skills.css';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <section className='skills-container'>
+      <Container style={{ width: '100vw', paddingTop: '2rem', paddingBottom: '2rem' }}>
+        <Typography variant="h1" align="center" style={{ paddingBottom: '2rem' }}>My Skills</Typography>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/javascript.svg" alt="javascript logo" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/java.svg" alt="java" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/html.svg" alt="html" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/css3.svg" alt="css" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/react.svg" alt="react" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/node-js.svg" alt="node" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/typescript.svg" alt="typescript" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/mongodb.svg" alt="mongodb" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/express.svg" alt="express" />
+          </Grid>
+          <Grid item xs={6} sm={4} md={3} lg={2}>
+            <img className='icon' src="/icons/git.svg" alt="git" />
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;
