4c4f755bcb3a65c38a975cb158544c232bb61360
Andres <AndresEM1524@gmail.com>
2023-06-25 11:58:51 -0700

    Added experience to index component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export {default as Navbar} from './Navbar';
 export {default as AboutMe} from './AboutMe';
 export {default as Hero} from './Hero';
-export {default as Experiences} from './Experiences';
+export {default as Experiences} from './Experience';
 export {default as Skills} from './Skills';
 export {default as Projects} from './Projects';
 export {default as Contact} from './Contact';
