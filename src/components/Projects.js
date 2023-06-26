e823ef6df9724f813e0fd2700f11e756ba513360
Andres <AndresEM1524@gmail.com>
2023-06-25 17:00:10 -0700

    Added starter content to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,12 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+import './Styles/Projects.css';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="projects-container">
+      <Typography variant="h2">My Projects</Typography>
+    </section>
   )
 }
 
