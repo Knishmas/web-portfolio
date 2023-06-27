63411234675b9b1b253c446c7bef92af839cc4ae
Andres <AndresEM1524@gmail.com>
2023-06-27 13:09:35 -0700

    Added meida query and grid to contact making it responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,31 +5,34 @@ import './Styles/Contact.css';
 const Contact = () => {
   return (
     <section id='contact-section' className="contact-container">
-      <Container sx={{ display: 'grid', gridTemplateColumns: '2fr 3fr'}}>
-      <Container className="contact-container__text" sx={{ textAlign: 'left'}}>
-        <Typography variant="h5" textAlign='left' style={{ paddingTop: '25px' }}> Contact</Typography>
-        <Typography variant="h3" textAlign='left' style={{ paddingTop: '25px' }}> Let's Chat!</Typography>
-        <Typography variant="h5" textAlign='left' style={{ paddingTop: '25px' }}> Feel free to reach out to me if you'd like to get in touch!</Typography>
-      </Container>
-
-        <div className="contact-container__form">
-          <form>
-            <Grid container direction="column" spacing={2}>
-              <Grid item>
-                <TextField required label="Full Name" sx={{ width: '600px' }} />
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" textAlign='left' style={{ paddingTop: '25px' }}> Contact</Typography>
+            <Typography variant="h3" textAlign='left' style={{ paddingTop: '25px' }}> Let's Chat!</Typography>
+            <Typography variant="h5" textAlign='left' style={{ paddingTop: '25px' }}> Feel free to reach out to me if you'd like to get in touch!</Typography>
+          </Grid>
+          <Grid item xs={12} md={6} className="form-container">
+            <form>
+              <Grid container direction="column" spacing={2}>
+                <Grid item>
+                  <TextField required label="Full Name" fullWidth />
+                </Grid>
+                <Grid item>
+                  <TextField required label="Email" fullWidth />
+                </Grid>
+                <Grid item>
+                  <TextField required label="Message" multiline rows={5} fullWidth />
+                </Grid>
+                <Grid item>
+                  <Button type="submit" variant="outlined" fullWidth>
+                    Send Away! ツ
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item>
-                <TextField required label="Email" sx={{ width: '600px' }} />
-              </Grid>
-              <Grid item>
-                <TextField required label="Message" multiline rows={5} sx={{ width: '600px' }} />
-              </Grid>
-              <Grid item>
-                <Button type="submit" variant="outlined" sx={{ width: '600px', height: '60px'}}>Send Away! ツ</Button>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
+            </form>
+          </Grid>
+        </Grid>
       </Container>
     </section>
   );
