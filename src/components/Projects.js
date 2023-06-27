5b020ab01b6d2a97fe4c490ef895dfd283bedbeb
Andres <AndresEM1524@gmail.com>
2023-06-27 12:30:25 -0700

    Added remaining project information as well as a grid for each card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Typography, Card, CardMedia, CardContent, Button, Container } from '@mui/material';
+import { Typography, Card, CardMedia, CardContent, Button, Container, Grid, Box } from '@mui/material';
 import React from 'react';
 import './Styles/Projects.css';
 
@@ -7,20 +7,20 @@ const Projects = () => {
     {
       title: 'Snap Companion',
       description: 'A personalized hub for directing Snapchatters to mental health resources through a personal and customizable companion',
-      image: './GIFs/sCompanion.gif',
+      image: './Project-assets/sCompanion.gif',
       githubLink: 'https://github.com/avrodzz/SnapCompanion',
     }, 
     {
-      title: 'Project 2',
-      description: 'project 2 description',
-      image: 'path/to/image.jpg', // Provide the correct path to the image file
-      githubLink: 'https://github.com/',
+      title: 'Rythm Radar',
+      description: 'A web application that gathers and displays your data such as top artists and songs over periods of time from Spotify!',
+      image: './Project-assets/RythmRadar.png',
+      githubLink: 'https://github.com/Knishmas/Spotify_API_v2',
     },
     {
-      title: 'Project 3',
-      description: 'project 3 description',
-      image: 'path/to/image.jpg', // Provide the correct path to the image file
-      githubLink: 'https://github.com/',
+      title: 'Eye Candy',
+      description: 'Fullstack web app, utilizing machine learning models, that can determine an article of clothing and its colors from a user image input.',
+      image: './Project-assets/EyeCandy.png', // Provide the correct path to the image file
+      githubLink: 'https://github.com/490-Team-MAAKDE/490-Project',
     }
   ];
 
@@ -28,7 +28,8 @@ const Projects = () => {
     <section className="projects-container">
       <Container>
         <Typography variant="h2">My Projects</Typography>
-        <div className="card-container">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
             <Card  className="project-card">
               <CardMedia
                 component="img"
@@ -37,18 +38,22 @@ const Projects = () => {
                 style={{ objectFit: 'cover' }}
                 image={projects[0].image}
                 alt={projects[0].title}
+                className="project-card-media"
               />
               <CardContent>
                 <Typography variant="h4">{projects[0].title}</Typography>
+                <Box display="flex" justifyContent="center">
                 <Typography variant="body1" className="project-description">
                   {projects[0].description}
                 </Typography>
+                </Box>
                 <Button variant="contained" href={projects[0].githubLink} target="_blank">
                   View on GitHub
                 </Button>
               </CardContent>
             </Card>
-      
+            </Grid>
+        <Grid item xs={12} sm={4} md={4} lg={4}>
           <Card className="project-card">
             <CardMedia
               component="img"
@@ -57,17 +62,22 @@ const Projects = () => {
               style={{ objectFit: 'cover' }}
               image={projects[1].image}
               alt={projects[1].title}
+              className="project-card-media"
             />
             <CardContent>
               <Typography variant="h4">{projects[1].title}</Typography>
+              <Box display="flex" justifyContent="center">
               <Typography variant="body1" className="project-description">
                 {projects[1].description}
               </Typography>
+              </Box>
               <Button variant="contained" href={projects[1].githubLink} target="_blank">
                 View on GitHub
               </Button>
             </CardContent>
           </Card>
+          </Grid>
+          <Grid item xs={12} sm={4} md={4} lg={4}>
           <Card className="project-card">
             <CardMedia
               component="img"
@@ -76,18 +86,22 @@ const Projects = () => {
               style={{ objectFit: 'cover' }}
               image={projects[2].image}
               alt={projects[2].title}
+              className="project-card-media"
             />
             <CardContent>
               <Typography variant="h4">{projects[2].title}</Typography>
+              <Box display="flex" justifyContent="center">
               <Typography variant="body1" className="project-description">
                 {projects[2].description}
               </Typography>
+              </Box>
               <Button variant="contained" href={projects[2].githubLink} target="_blank">
                 View on GitHub
               </Button>
             </CardContent>
           </Card>
-        </div>
+          </Grid>
+          </Grid>
       </Container>
     </section>
   );
