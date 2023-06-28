941e71cc74a310f32387e8816d677d04db4a8dce
Andres <AndresEM1524@gmail.com>
2023-06-27 19:07:23 -0700

    Added grid and description typography breakpoints for responsiveness

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { Container, Typography, Grid } from '@mui/material';
 import './Styles/AboutMe.css';
-import { Container, Typography } from '@mui/material';
+
 const AboutMe = () => {
   return (
     <section id="about-me-section" className="about-container">
-      <Container sx={{ display: 'grid', gridTemplateColumns: '3fr 2fr' }} alignItems="center">
-      <div className="about-container__text">
-      <Typography className='about-container__text__title' fontWeight="bold" variant="h6" textAlign='left' >About Me </Typography>
-        <Typography variant="h3" textAlign='left'>
-          Hi, I'm Andres. I'm a passionate and dedicated fullstack web developer.
-          I'm constantly learning and curiously exploring new technologies. 
-          When I'm not coding, you can find me making music, at the gym, at concerts, building mechanical keyboards, and gaming!
-        </Typography>
-      </div>
-      
-      <img src={require('../assets/decoration.png')} style={{ width: '500px' }}  alt="about-decoration" />
+      <Container>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <div className="about-container__text">
+              <Typography className='about-container__text__title' fontWeight="bold" variant="h6" textAlign='left'>About Me</Typography>
+              <Typography variant="h3" textAlign='left' sx={{ typography: { sm: "h4", xs: "h5" } }}>
+                Hi, I'm Andres. I'm a passionate and dedicated fullstack web developer.
+                I'm constantly learning and curiously exploring new technologies.
+                When I'm not coding, you can find me making music, at the gym, at concerts, building mechanical keyboards, and gaming!
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <img src={require('../assets/decoration.png')} className="about-image" alt="about-decoration" />
+          </Grid>
+        </Grid>
       </Container>
-     </section>
+    </section>
   );
 };
 
