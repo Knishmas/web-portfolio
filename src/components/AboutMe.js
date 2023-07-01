c9c026ce9ae7b6c180b2c2897d7ddfdaf608c857
Andres <AndresEM1524@gmail.com>
2023-06-30 23:44:33 -0700

    Changed about me header color

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => {
         <Grid container spacing={2} alignItems="center">
           <Grid item xs={12} md={6}>
             <div className="about-container__text">
-              <Typography className='about-container__text__title' fontWeight="bold" variant="h6" textAlign='left'>About Me</Typography>
+              <Typography className='about-container__text__title' fontWeight="bold" variant="h6"  style={{ color: '#03DAC5' }} textAlign='left'>About Me</Typography>
               <Typography variant="h3" textAlign='left' sx={{ typography: { sm: "h4", xs: "h5" } }}>
                 Hi, I'm Andres. I'm a passionate and dedicated fullstack web developer.
                 I'm constantly learning and curiously exploring new technologies.
