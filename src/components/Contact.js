22c644f70e0a2943258914526f95f374a543640c
Andres <AndresEM1524@gmail.com>
2023-07-16 09:52:02 -0700

    Added imports for emailjs & useRef and added sendemail function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { TextField, Button, Typography, Container, Grid } from '@mui/material';
 import './Styles/Contact.css';
 
 const Contact = () => {
+
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
   return (
     <section id='contact-section' className="contact-container">
       <Container>
