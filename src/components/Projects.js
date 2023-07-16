73e58e9345e39a03d89cca060027ffdac89439a8
Andres <AndresEM1524@gmail.com>
2023-07-15 17:30:59 -0700

    Modified img height to 60%,card box removed, img assets changed, button color changed, and border radius

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,19 +7,19 @@ const Projects = () => {
     {
       title: 'Snap Companion',
       description: 'A prototype feature directing Snapchatters to mental health resources through a personal and customizable companion',
-      image: './Project-assets/sCompanion.gif',
+      image: './Project-assets/sCompanion-Cropped2.png',
       githubLink: 'https://github.com/avrodzz/SnapCompanion',
     }, 
     {
       title: 'Rythm Radar',
       description: 'A web application that gathers and displays your data such as top artists and songs over periods of time from Spotify!',
-      image: './Project-assets/RythmRadar.png',
+      image: './Project-assets/RythmRadar-Cropped.png',
       githubLink: 'https://github.com/Knishmas/Spotify_API_v2',
     },
     {
       title: 'Eye Candy',
-      description: 'Fullstack web app, utilizing machine learning models, that can determine an article of clothing and its colors from a user input.',
-      image: './Project-assets/EyeCandy.png', // Provide the correct path to the image file
+      description: 'Fullstack web app, utilizing machine learning, that can determine an article of clothing and its colors from a user input.',
+      image: './Project-assets/EyeCandy-Cropped.png', // Provide the correct path to the image file
       githubLink: 'https://github.com/490-Team-MAAKDE/490-Project',
     }
   ];
@@ -27,78 +27,75 @@ const Projects = () => {
   return (
     <section id='project-section' className="projects-container">
       <Container>
-        <Typography variant="h2"  style={{color: '#03DAC5'}}>My Projects</Typography>
+        <Typography variant="h4"  style={{color: '#03DAC5', marginBottom: '0.rem'}}>My Projects</Typography>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} md={4}>
-            <Card  className="project-card">
+            <Card  className="project-card" style={{ borderRadius: '8px' }}>
               <CardMedia
                 component="img"
-                height="500"
-                width="200"
-                style={{ objectFit: 'cover' }}
+                height="200"
+                width="500"
+                style={{ objectFit: 'cover', height: '60%' }}
                 image={projects[0].image}
                 alt={projects[0].title}
                 className="project-card-media"
               />
-              <CardContent>
-                <Typography variant="h4" style={{ color: "black" }}>{projects[0].title} </Typography>
-                <Box display="flex" justifyContent="center">
-                <Typography variant="body1" className="project-description" style={{ color: "black" }}>
-                  {projects[0].description}
-                </Typography>
-                </Box>
-                <Button variant="contained" href={projects[0].githubLink} target="_blank">
-                  View on GitHub
-                </Button>
-              </CardContent>
+             <CardContent style={{ backgroundColor: '#4F4557', textAlign: 'left', paddingLeft: '4rem' }}>
+              <Typography variant="h4" style={{ color: 'white' }}>{projects[0].title}</Typography>
+              <Typography variant="body1" className="project-description" style={{ color: 'white', marginBottom: '0.9rem', textAlign: 'left' }}>
+                {projects[0].description}
+              </Typography>
+              <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0' }} target="_blank">
+                View on GitHub
+              </Button>
+            </CardContent>
             </Card>
             </Grid>
         <Grid item xs={12} sm={4} md={4} lg={4}>
-          <Card className="project-card">
+        <Card  className="project-card" style={{ borderRadius: '8px' }}>
             <CardMedia
               component="img"
-              height="500"
-              width="200"
-              style={{ objectFit: 'cover' }}
+              
+              height="200"
+              width="300"
+              style={{ objectFit: 'cover', height: '60%'}}
               image={projects[1].image}
               alt={projects[1].title}
               className="project-card-media"
             />
-            <CardContent>
-              <Typography variant="h4" style={{ color: "black" }}>{projects[1].title}</Typography>
-              <Box display="flex" justifyContent="center">
-              <Typography variant="body1" className="project-description" style={{ color: "black" }}>
+            <CardContent style={{ backgroundColor: '#4F4557', textAlign: 'left', paddingLeft: '4rem' }}>
+              <Typography variant="h4" style={{ color: 'white' }}>{projects[1].title}</Typography>
+              <Typography variant="body1" className="project-description" style={{ color: 'white', marginBottom: '0.9rem', textAlign: 'left' }}>
                 {projects[1].description}
               </Typography>
-              </Box>
-              <Button variant="contained" href={projects[1].githubLink} target="_blank">
+              <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0' }} target="_blank">
                 View on GitHub
               </Button>
             </CardContent>
+
           </Card>
           </Grid>
           <Grid item xs={12} sm={4} md={4} lg={4}>
-          <Card className="project-card">
+          <Card  className="project-card" style={{ borderRadius: '8px' }}>
             <CardMedia
               component="img"
-              height="500"
-              width="200"
-              style={{ objectFit: 'cover' }}
+              height="200"
+              width="300"
+              style={{ objectFit: 'cover', height: '60%' }}
               image={projects[2].image}
               alt={projects[2].title}
               className="project-card-media"
             />
-            <CardContent>
-              <Typography variant="h4" style={{ color: "black" }}>{projects[2].title}</Typography>
-              <Box display="flex" justifyContent="center">
-              <Typography variant="body1"  className="project-description" style={{ color: "black" }}>
-                {projects[2].description}
-              </Typography>
-              </Box>
-              <Button variant="contained" href={projects[2].githubLink} target="_blank">
-                View on GitHub
-              </Button>
-            </CardContent>
+            <CardContent style={{ backgroundColor: '#4F4557', textAlign: 'left',paddingLeft: '4rem'  }}>
+  <Typography variant="h4" style={{ color: 'white' }}>{projects[2].title}</Typography>
+  <Typography variant="body1" className="project-description" style={{ color: 'white', marginBottom: '0.9rem', textAlign: 'left' }}>
+    {projects[2].description}
+  </Typography>
+  <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0' }} target="_blank">
+    View on GitHub
+  </Button>
+</CardContent>
+
           </Card>
           </Grid>
           </Grid>
