13368b029b49bebf7c9da8a4174fa45edfb4901c
Andres <AndresEM1524@gmail.com>
2023-07-18 14:45:30 -0700

    Added react-spinners for onload animation.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import { AboutMe,Hero,Skills,Contact,Experiences, Projects} from './components';
+import React, { useState, useEffect } from 'react';
+import { AboutMe, Hero, Skills, Contact, Experiences, Projects } from './components';
+import { ScaleLoader
+
+} from 'react-spinners';
+
 import './App.css';
 
 function App() {
+  const [loadingInProgress, setLoadingInProgress] = useState(true);
+
+  useEffect(() => {
+    window.onload = () => {
+      setLoadingInProgress(false);
+    };
+  }, []);
+
   return (
     <div className="App">
-        <Hero/>
-        <AboutMe/>
-        <Experiences/>
-        <Skills/>
-        <Projects/>
-        <Contact/>
+      {loadingInProgress ? (
+        <div className="loading-container">
+          <ScaleLoader
+          color="#36d7b7"
+          height={70}
+          margin={4}
+          radius={4}
+          width={8}
+        />
+        </div>
+      ) : (
+        <>
+          <Hero />
+          <AboutMe />
+          <Experiences />
+          <Skills />
+          <Projects />
+          <Contact />
+        </>
+      )}
     </div>
-  
   );
 }
 
