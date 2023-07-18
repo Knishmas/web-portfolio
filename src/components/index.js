a6b620d5f31743c7b96e52b618118093bb68c969
Andres <AndresEM1524@gmail.com>
2023-07-17 17:25:22 -0700

    Added animated  component to index.

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export {default as Experiences} from './Experience';
 export {default as Skills} from './Skills';
 export {default as Projects} from './Projects';
 export {default as Contact} from './Contact';
+export {default as Animated} from './Animated';
