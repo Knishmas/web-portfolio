314d26b06ea46f10c2b75138adf816719addd431
Andres <AndresEM1524@gmail.com>
2023-07-18 12:52:38 -0700

    Deleted resume description & modified padding.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,11 +32,10 @@ const Contact = () => {
         <Animated>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
-            <Typography variant="h6" textAlign='left' style={{ paddingTop: '25px', color: '#03DAC5' }}> Contact</Typography>
+            <Typography variant="h6" textAlign='left' style={{ paddingTop: '25px', color: '#03DAC5', fontWeight: 'bold' }}> Contact</Typography>
             <Typography variant="h3" textAlign='left' style={{ paddingTop: '25px' }}> Let's Chat!</Typography>
             <Typography variant="h5" textAlign='left' style={{ paddingTop: '25px' }}> Feel free to reach out to me if you'd like to get in touch!</Typography>
-            <Typography variant="body1" textAlign='left' style={{ paddingTop: '25px', textDecoration: 'none' }}> <a href='mailto:AndresEM1524@gmail.com' className="emailLink">✉️ AndresEM1524@gmail.com</a></Typography>
-            <Typography variant="body1" textAlign='left' style={{ paddingTop: '25px', paddingBottom: '25px' }}> Learn about my experiences with my Resume!</Typography>
+            <Typography variant="body1" textAlign='left' style={{ paddingTop: '25px', textDecoration: 'none', paddingBottom: '63px' }}> <a href='mailto:AndresEM1524@gmail.com' className="emailLink">✉️ AndresEM1524@gmail.com</a></Typography>
             <Button variant="contained"  color="primary" href="https://drive.google.com/file/d/1wSEUO-KF9K7ondjv09T4fyrjYr0puTLe/view?usp=sharing" style={{ backgroundColor: '#6002E0', color: 'white', float: 'left' }} target="_blank">
               Resume Ready!
             </Button>
