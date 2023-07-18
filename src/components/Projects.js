58bbe188ec9ddeecab80eebc2743145cdcf78c87
Andres <AndresEM1524@gmail.com>
2023-07-17 17:24:51 -0700

    Modiefied padding, fontweight/size, and grid item responsiveness.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
-import { Typography, Card, CardMedia, CardContent, Button, Container, Grid, Box } from '@mui/material';
+import { Typography, Card, CardMedia, CardContent, Button, Container, Grid } from '@mui/material';
 import React from 'react';
 import './Styles/Projects.css';
+import Animated from './Animated';
 
 const Projects = () => {
   const projects = [
@@ -27,7 +28,8 @@ const Projects = () => {
   return (
     <section id='project-section' className="projects-container">
       <Container>
-        <Typography variant="h4"  style={{color: '#03DAC5', marginBottom: '0.rem'}}>My Projects</Typography>
+        <Animated>
+        <Typography variant="h3"  style={{color: '#03DAC5', marginBottom: '2rem'}}>My Projects</Typography>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} md={4}>
             <Card  className="project-card" style={{ borderRadius: '8px' }}>
@@ -40,18 +42,18 @@ const Projects = () => {
                 alt={projects[0].title}
                 className="project-card-media"
               />
-             <CardContent style={{ backgroundColor: '#4F4557', textAlign: 'left', paddingLeft: '4rem' }}>
-              <Typography variant="h4" style={{ color: 'white' }}>{projects[0].title}</Typography>
+             <CardContent style={{ backgroundColor: '#233353', textAlign: 'left', paddingLeft: '4rem' }}>
+              <Typography variant="body2" style={{ color: 'white', fontWeight: 'bold', fontSize: '2.05rem' }}>{projects[0].title}</Typography>
               <Typography variant="body1" className="project-description" style={{ color: 'white', marginBottom: '0.9rem', textAlign: 'left' }}>
                 {projects[0].description}
               </Typography>
-              <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0' }} target="_blank">
+              <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0', fontWeight: 'bold',  width: '85%' }} target="_blank">
                 View on GitHub
               </Button>
             </CardContent>
             </Card>
             </Grid>
-        <Grid item xs={12} sm={4} md={4} lg={4}>
+        <Grid item xs={12} sm={6} md={4}>
         <Card  className="project-card" style={{ borderRadius: '8px' }}>
             <CardMedia
               component="img"
@@ -63,19 +65,19 @@ const Projects = () => {
               alt={projects[1].title}
               className="project-card-media"
             />
-            <CardContent style={{ backgroundColor: '#4F4557', textAlign: 'left', paddingLeft: '4rem' }}>
-              <Typography variant="h4" style={{ color: 'white' }}>{projects[1].title}</Typography>
+            <CardContent style={{ backgroundColor: '#233353', textAlign: 'left', paddingLeft: '4rem' }}>
+              <Typography variant="body2" style={{ color: 'white', fontWeight: 'bold', fontSize: '2.2rem' }}>{projects[1].title}</Typography>
               <Typography variant="body1" className="project-description" style={{ color: 'white', marginBottom: '0.9rem', textAlign: 'left' }}>
                 {projects[1].description}
               </Typography>
-              <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0' }} target="_blank">
+              <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0', fontWeight: 'bold', width: '85%' }} target="_blank">
                 View on GitHub
               </Button>
             </CardContent>
 
           </Card>
           </Grid>
-          <Grid item xs={12} sm={4} md={4} lg={4}>
+          <Grid item xs={12} sm={6} md={4}>
           <Card  className="project-card" style={{ borderRadius: '8px' }}>
             <CardMedia
               component="img"
@@ -86,12 +88,12 @@ const Projects = () => {
               alt={projects[2].title}
               className="project-card-media"
             />
-            <CardContent style={{ backgroundColor: '#4F4557', textAlign: 'left',paddingLeft: '4rem'  }}>
-  <Typography variant="h4" style={{ color: 'white' }}>{projects[2].title}</Typography>
+            <CardContent style={{ backgroundColor: '#233353', textAlign: 'left',paddingLeft: '4rem'  }}>
+  <Typography variant="body2" style={{ color: 'white', fontWeight: 'bold', fontSize: '2.2rem' }}>{projects[2].title}</Typography>
   <Typography variant="body1" className="project-description" style={{ color: 'white', marginBottom: '0.9rem', textAlign: 'left' }}>
     {projects[2].description}
   </Typography>
-  <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0' }} target="_blank">
+  <Button variant="contained" href={projects[0].githubLink} style={{ backgroundColor: '#6002E0',fontWeight: 'bold',  width: '85%' }} target="_blank">
     View on GitHub
   </Button>
 </CardContent>
@@ -99,6 +101,7 @@ const Projects = () => {
           </Card>
           </Grid>
           </Grid>
+          </Animated>
       </Container>
     </section>
   );
